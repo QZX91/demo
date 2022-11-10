c2c7721bbb512ab012caead4992123e29d3ad000
QZX91 <104272197+QZX91@users.noreply.github.com>
2022-11-10 22:52:29 +0800

    delete some files

--- jr-todos/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const taskRouter = require('./tasks');
-
-const v1Router = express.Router();
-
-v1Router.use('/tasks', taskRouter);
-
-module.exports = v1Router;
--- react part2/react-review-app/src/components/UI/Card/Card.js
+++
@@ -1,7 +0,0 @@
-import classes from "./Card.module.css";
-
-const Card = (props) => {
-  return <div className={`${classes.card} ${props.extraClass}`}>{props.children}</div>;
-};
-
-export default Card;
